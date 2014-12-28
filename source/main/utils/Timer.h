e6019d0c8a55757609dfbff7c73a532aa4c95f4e
only_a_ptr <petr@ohlidal.cz>
2014-12-28 14:21:01 +0100

    :triangular_ruler: Class PrecisionTimer improved

--- source/main/utils/Timer.h
+++ source/main/utils/Timer.h
@@ -1,12 +1,27 @@
-// license: do whatever you want to do with it ;)
+/*
+	This source file is part of Rigs of Rods
+	Copyright 2005-2012 Pierre-Michel Ricordel
+	Copyright 2007-2012 Thomas Fischer
+	Copyright 2013-2015 Petr Ohlidal
+
+	For more information, see http://www.rigsofrods.com/
+
+	Rigs of Rods is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License version 3, as
+	published by the Free Software Foundation.
+
+	Rigs of Rods is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Rigs of Rods. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
-#ifndef TIMER_H__
-#define TIMER_H__
 
 #include "RoRPrerequisites.h"
-
-// boost timer is awful, measures cpu time on linux only...
-// thus we have to hack together some cross platform timer :(
 
 #ifndef _WIN32
 #include <sys/time.h>
@@ -16,12 +31,6 @@
 
 class PrecisionTimer : public ZeroedMemoryAllocator
 {
-protected:
-#ifdef _WIN32
-    LARGE_INTEGER start;
-#else
-    struct timeval start;
-#endif // _WIN32
 
 public:
     PrecisionTimer()
@@ -29,16 +38,39 @@ public:
         restart();
     }
 
+	long long ElapsedMilliseconds()
+	{
+#ifdef WIN32
+		LARGE_INTEGER tick;
+		QueryPerformanceCounter(&tick);
+		return (tick.QuadPart - m_start_timestamp) / (m_ticks_per_second / 1000);
+#else
+		struct timeval now;
+        gettimeofday(&now, NULL); // TODO: Known to be inaccurate, use clock_gettime()
+        return ((now.tv_sec - start.tv_sec)*1000) + ((now.tv_usec - start.tv_usec)/1000.0);
+#endif
+	}
+
+	long long ElapsedTicks()
+	{
+#ifdef WIN32
+		LARGE_INTEGER tick;
+		QueryPerformanceCounter(&tick);
+		return (tick.QuadPart - m_start_timestamp);
+#else
+		return 0; // TODO
+#endif
+	}
+
     double elapsed()
     {
 #ifdef _WIN32
-        LARGE_INTEGER tick, ticksPerSecond;
-        QueryPerformanceFrequency(&ticksPerSecond);
+        LARGE_INTEGER tick;
         QueryPerformanceCounter(&tick);
-        return ((double)tick.QuadPart - (double)start.QuadPart) / (double)ticksPerSecond.QuadPart;
+        return ((double)tick.QuadPart - (double)m_start_timestamp) / (double)m_ticks_per_second;
 #else
         struct timeval now;
-        gettimeofday(&now, NULL);
+        gettimeofday(&now, NULL); // TODO: Known to be inaccurate, use clock_gettime()
         return (now.tv_sec - start.tv_sec) + (now.tv_usec - start.tv_usec)/1000000.0;
 #endif // _WIN32
     }
@@ -46,11 +78,21 @@ public:
     void restart()
     {
 #ifdef _WIN32
+		LARGE_INTEGER start, ticks_per_second;
         QueryPerformanceCounter(&start);
+		QueryPerformanceFrequency(&ticks_per_second);
+		m_start_timestamp = start.QuadPart;
+		m_ticks_per_second = ticks_per_second.QuadPart;
 #else
         gettimeofday(&start, NULL);
 #endif // _WIN32
     }
-};
 
-#endif //TIMER_H__
+private:
+#ifdef WIN32
+    long long m_start_timestamp;
+	long long m_ticks_per_second;
+#else
+    struct timeval m_start;
+#endif
+};
