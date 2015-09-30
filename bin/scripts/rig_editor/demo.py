3a58b51e4d8e507424dbeb731efda8696a7e0781
Petr Ohlidal <petr@ohlidal.cz>
2015-09-30 23:46:48 +0200

    [add] RigDef/Python: Node::Id, NodeDefaults

--- bin/scripts/rig_editor/demo.py
+++ bin/scripts/rig_editor/demo.py
@@ -19,6 +19,13 @@ class Demo:
         
     def test_truckfile_import(directory, filename):
         import ror_truckfile
+        
+        def print_node(node):
+            text = "\tNode | id: " + node.id.to_str()
+            text += ", x: " + str(node.position.x)
+            text += ", flag_n: " + str(node.option_n)
+            text += ", flag_l: " + str(node.option_l)
+            print(text) 
     
         parser = ror_truckfile.Parser()
         parser.parse_file(directory, filename)
@@ -30,4 +37,11 @@ class Demo:
         print("name:", m.name)
         m.name = "NameTest" # Does string assignment work?
         print("name test: [" + truck.root_module.name + "]")
-        print("num nodes: ", len(m.nodes))      
+        print("num nodes: ", len(m.nodes))
+        for node in m.nodes:
+            print_node(node)
+        # Does property assignment work?
+        n = m.nodes[0]
+        print("node flag test A: ", n.option_x)
+        m.nodes[0].option_x = True
+        print("node flag test B: ", m.nodes[0].option_x)   
--- source/rig_file_input_output/RigDef_File.h
+++ source/rig_file_input_output/RigDef_File.h
@@ -91,6 +91,21 @@ struct NodeDefaults
 	float volume;
 	float surface;
 	unsigned int options; ///< Bit flags
+
+	// IMPORTANT!
+	// These definitions _MUST_ match definitions in RigDef::Node
+	BITMASK_PROPERTY( options,  1, OPTION_n_MOUSE_GRAB        , HasFlag_n, SetFlag_n)
+	BITMASK_PROPERTY( options,  2, OPTION_m_NO_MOUSE_GRAB     , HasFlag_m, SetFlag_m)
+	BITMASK_PROPERTY( options,  3, OPTION_f_NO_SPARKS         , HasFlag_f, SetFlag_f)
+	BITMASK_PROPERTY( options,  4, OPTION_x_EXHAUST_POINT     , HasFlag_x, SetFlag_x)
+	BITMASK_PROPERTY( options,  5, OPTION_y_EXHAUST_DIRECTION , HasFlag_y, SetFlag_y)
+	BITMASK_PROPERTY( options,  6, OPTION_c_NO_GROUND_CONTACT , HasFlag_c, SetFlag_c)
+	BITMASK_PROPERTY( options,  7, OPTION_h_HOOK_POINT        , HasFlag_h, SetFlag_h)
+	BITMASK_PROPERTY( options,  8, OPTION_e_TERRAIN_EDIT_POINT, HasFlag_e, SetFlag_e)
+	BITMASK_PROPERTY( options,  9, OPTION_b_EXTRA_BUOYANCY    , HasFlag_b, SetFlag_b)
+	BITMASK_PROPERTY( options, 10, OPTION_p_NO_PARTICLES      , HasFlag_p, SetFlag_p)
+	BITMASK_PROPERTY( options, 11, OPTION_L_LOG               , HasFlag_L, SetFlag_L)
+	BITMASK_PROPERTY( options, 12, OPTION_l_LOAD_WEIGHT       , HasFlag_l, SetFlag_l)
 };
 
 /* -------------------------------------------------------------------------- */
