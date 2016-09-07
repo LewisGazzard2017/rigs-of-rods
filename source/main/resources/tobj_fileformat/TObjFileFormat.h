9a5a3502998226a2c31cac914f649eaf8b8581dc
only_a_ptr <petr@ohlidal.cz>
2016-09-07 11:10:24 +0200

    WIP +grass

--- source/main/resources/tobj_fileformat/TObjFileFormat.h
+++ source/main/resources/tobj_fileformat/TObjFileFormat.h
@@ -4,33 +4,63 @@
 struct TObjTree
 {                     
     TObjTree():
-        yaw_from    (0.f),
-        yaw_to      (0.f),
-        scale_from  (0.f),
-        scale_to    (0.f),
+        yaw_from    (0.f),        yaw_to      (0.f),
+        scale_from  (0.f),        scale_to    (0.f),
+        min_distance(90),         max_distance(700),
         high_density(1.f),
-        min_distance(90),
-        max_distance(700),
         grid_spacing(0.f),
         
-        tree_mesh          [0]('\0'),
-        color_map          [0]('\0'),
-        density_map        [0]('\0'),
-        tree_collision_mesh[0]('\0'),
+        tree_mesh     [0]('\0'),
+        color_map     [0]('\0'),
+        density_map   [0]('\0'),
+        collision_mesh[0]('\0'),
     {}
     
-    float    yaw_from;
-    float    yaw_to;
-    float    scale_from;
-    float    scale_to;
+    float    yaw_from,        yaw_to;
+    float    scale_from,      scale_to;
+    float    min_distance,    max_distance;        
     float    high_density;
-    float    min_distance;
-    float    max_distance;
     float    grid_spacing;
+    
     char     tree_mesh[300];
     char     color_map[300];
     char     density_map[300];
-    char     tree_collision_mesh[300];   
+    char     collision_mesh[300];   
+};
+
+/// Unified 'grass' and 'grass2'
+struct TObjGrass
+{
+    TObjGrass():
+        range(80),
+        technique(GRASSTECH_CROSSQUADS),
+        grow_techniq(0),
+        sway_speed(0.5f),
+        sway_length(0.05f),
+        sway_distrib(10.f),
+        
+        min_x(0.2f),   min_y(0.2f),   min_h(-9999.f),
+        max_x(1.0f),   max_y(0.6f),   max_h(+9999.f),
+        
+        grass_mat_name       [0]('\0'),
+        color_map_filename   [0]('\0'),
+        density_map_filename [0]('\0')
+    {}
+    
+    int      range;
+    int      technique;
+    int      grow_techniq;
+    float    sway_speed;
+    float    sway_length;
+    float    sway_distrib;
+    float    density;
+    
+    float    min_x,   min_y,   min_h;
+    float    max_x,   max_y,   max_h;
+    
+    char     grass_mat_name[300];
+    char     color_map_filename[300];
+    char     density_map_filename[300];
 };
 
 struct TObjFile
@@ -45,6 +75,7 @@ struct TObjFile
     Ogre::Vector3           grid_position;
     bool                    grid_enabled;
     std::vector<TObjTree>   trees;
+    std::vector<TObjGrass>  grass;
 };
 
 } // namespace TObj
